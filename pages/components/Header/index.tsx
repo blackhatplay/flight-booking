7086c0621bcb9889ee7f0d6f43555f2b9d32a7a0
blackhatplay <mannuall4u@gmail.com>
2020-12-30 23:31:52 +0530

    first vercel

--- pages/components/Header/index.tsx
+++ pages/components/Header/index.tsx
@@ -31,7 +31,7 @@ const Header = ()=> {
                     <div className="row">
                         <div className="block">
                             <h1>
-                            <span>Okay</span>
+                            <span>SEARCH</span>
                             </h1>
                         </div>
                     </div>
