d0916054d91861032a985a437b9d167e6f4c90a0
blackhatplay <mannuall4u@gmail.com>
2021-02-07 10:53:38 +0530

    favicon update

--- src/components/Head/index.tsx
+++ src/components/Head/index.tsx
@@ -4,19 +4,7 @@ const index = (props) => {
     return (
         <Head>
             <title>{props.title}</title>
-            {/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-L59BNP8FW0"></script>
-            <script
-                dangerouslySetInnerHTML={{
-                    __html: `
 
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-          
-            gtag('config', 'G-L59BNP8FW0');
-              `,
-                }}
-            /> */}
         </Head>
     )
 }
